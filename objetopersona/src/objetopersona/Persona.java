54d36e9d233fbbb0366cc9ab820fafb4bfb7bb61
alumno <alumno@aula1-01>
2018-11-12 18:38:28 +0100

    terminando el objeto persona

--- objetopersona/src/objetopersona/Persona.java
+++ objetopersona/src/objetopersona/Persona.java
@@ -12,43 +12,67 @@ package objetopersona;
 public class Persona {
     private final String HOMBRE = "H";
     private final String MUJER = "M";
+    private final int PINF = -1;
+    private final int PN = 0;
+    private final int PO = +1;
     private String nombre; 
     private int edad;
     private String dni;
     private String sexo;
     private int peso;
     private int altura;
+    private final boolean ME=false;
+    
+    
+        public Persona() {
+            nombre="";
+            edad=0;
+            dni=generarDNI();
 
-    public Persona() {
-        nombre="";
-        edad=0;
-        dni=generarDNI();
-        
-        sexo=HOMBRE;
-        peso=0;
-        altura=0;
-                
-    }
+            sexo=HOMBRE;
+            peso=0;
+            altura=0;
 
-    public Persona(String nombre, int edad, String dni, String sexo, int peso, int altura) {
-        this.nombre = nombre;
-        this.edad = edad;
-        this.dni = dni;
-        this.sexo = sexo;
-        this.peso = peso;
-        this.altura = altura;
-    }
-     public Persona(String nombre, int edad,  String sexo) {
-        this.nombre = nombre;
-        this.edad = edad;
-        this.dni = dni;
-        this.sexo = sexo;
-        this.peso = peso;
-        this.altura = altura;
-    }
+        }
 
+        public Persona(String nombre, int edad, String dni, String sexo, int peso, int altura) {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.dni = dni;
+            this.sexo = sexo;
+            this.peso = peso;
+            this.altura = altura;
+        }
+        public Persona(String nombre, int edad) {
+           this.nombre = nombre;
+           this.edad = edad;
+           this.dni = dni;
+           this.sexo = sexo;
+           this.peso = peso;
+           this.altura = altura;
+        }
 
+            public int calcularIMC(){
+            int imc;
+            imc=peso/(altura*altura);
+                if (imc<18.5) {
+                    return PINF;
+                }else if (imc>30) {
+                    return PO;
 
+                }else
+                    return PN;
+
+            }
+            public boolean esMayorDeEdad(){
+                if (edad>=18) {
+                    ME=true;
+                }else
+                    ME=false;
+            }
+            public void generarDNI(){
+            
+            }
 }
 
 
